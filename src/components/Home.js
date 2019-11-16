3820a590f1db3a8c454dd890465b2e083c11e13f
SiddantGurung <siddantgurung@siddantrungsmbp.home>
2019-11-16 22:23:57 +0000

    new build

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,98 +1,103 @@
-import React from 'react'
-import RegisterForm from './auth/RegisterForm'
-import LoginForm from './auth/LoginForm'
-import axios from 'axios'
-import Auth from '../lib/Auth'
-import Flash from '../lib/Flash'
+import React from "react";
+import RegisterForm from "./auth/RegisterForm";
+import LoginForm from "./auth/LoginForm";
+import axios from "axios";
+import Auth from "../lib/Auth";
+import Flash from "../lib/Flash";
 
-
-class Home extends React.Component{
-
-  constructor(){
-    super()
+class Home extends React.Component {
+  constructor() {
+    super();
     this.state = {
       data: {},
       register: true,
       error: {}
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.changeState = this.changeState.bind(this)
-
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeState = this.changeState.bind(this);
   }
 
-  handleChange({target: {name, value}}){
-    const data = {...this.state.data, [name]: value }
-    const errors = {...this.state.errors, [name]: null }
-    this.setState({ data, errors })
+  handleChange({ target: { name, value } }) {
+    const data = { ...this.state.data, [name]: value };
+    const errors = { ...this.state.errors, [name]: null };
+    this.setState({ data, errors });
   }
 
-  registerFunction(){
+  registerFunction() {
     axios
-      .post('api/register', this.state.data)
+      .post("api/register", this.state.data)
       .then(res => {
-        Flash.setMessage('success', res.data.message)
-        this.setState({...this.state,  data: {}, register: false})
-        this.props.history.push('/')
+        Flash.setMessage("success", res.data.message);
+        this.setState({ ...this.state, data: {}, register: false });
+        this.props.history.push("/");
       })
-      .catch(err =>this.setState({...this.state, error: err.response.data }))
+      .catch(err => this.setState({ ...this.state, error: err.response.data }));
   }
 
-  changeState(){
-    this.setState({...this.state, register: !this.state.register })
+  changeState() {
+    this.setState({ ...this.state, register: !this.state.register });
   }
 
-  handleSubmit(e){
-    e.preventDefault(e)
-    const command = e.target.name
-    if(command === 'register' ) this.registerFunction()
-    else this.loginFunction()
+  handleSubmit(e) {
+    e.preventDefault(e);
+    const command = e.target.name;
+    if (command === "register") this.registerFunction();
+    else this.loginFunction();
   }
 
-  componentDidMount(){
-    this.setState({...this.state, register: this.props.register })
+  componentDidMount() {
+    this.setState({ ...this.state, register: this.props.register });
   }
 
-  render(){
-    console.log(Auth.isAuthenticated())
-    const authenticated = Auth.isAuthenticated()
+  render() {
+    console.log(Auth.isAuthenticated());
+    const authenticated = Auth.isAuthenticated();
     if (!authenticated) {
-      return(
+      return (
         <section className="section">
           <div className="container">
             <div className="columns">
               <div className="column is-half authorisation">
                 <div className="title is-1">Pepino</div>
-                <h2  className="title is-2">Built for developers</h2>
-                <div  className="homeText">If you want to manage your projects, see what packages are out there and find out what other users are doing.</div>
-                <div  className="homeText">
-                Connect with other developers, Explore all possibilities.
+                <h2 className="title is-2">Built for developers</h2>
+                <div className="homeText">
+                  If you want to manage your projects, see what packages are out
+                  there and find out what other users are doing.
                 </div>
-                <div  className="homeText">
-                  Join Today.
+                <div className="homeText">
+                  Connect with other developers, Explore all possibilities.
+                  teting stuff
                 </div>
+                <div className="homeText">Join Today.</div>
               </div>
               <div className="column is-half  authorisation">
-                {this.state.register &&  <RegisterForm
-                  handleSubmit={this.handleSubmit}
-                  handleChange={this.handleChange}
-                  data={this.state.data}
-                  errors={this.state.error}
-                  changeState={this.changeState}
-                />}
+                {this.state.register && (
+                  <RegisterForm
+                    handleSubmit={this.handleSubmit}
+                    handleChange={this.handleChange}
+                    data={this.state.data}
+                    errors={this.state.error}
+                    changeState={this.changeState}
+                  />
+                )}
 
-                {!this.state.register && <LoginForm
-                  handleSubmit={(e)=>this.props.handleLogin(e, this.state.data)}
-                  handleChange={this.handleChange}
-                  data={this.state.data}
-                  changeState={this.changeState}
-                  errors={this.props.error}
-                />}
+                {!this.state.register && (
+                  <LoginForm
+                    handleSubmit={e =>
+                      this.props.handleLogin(e, this.state.data)
+                    }
+                    handleChange={this.handleChange}
+                    data={this.state.data}
+                    changeState={this.changeState}
+                    errors={this.props.error}
+                  />
+                )}
               </div>
             </div>
           </div>
         </section>
-      )
+      );
     }
     if (authenticated) {
       return (
@@ -102,12 +107,15 @@ class Home extends React.Component{
               <div className="pepino-logo"></div>
               <div className="title is-1">LASER-BLADE</div>
             </div>
-            <div className="blurb">If you want to manage your projects, see what packages are out there and find out what other users are doing blah blah blah </div>
+            <div className="blurb">
+              If you want to manage your projects, see what packages are out
+              there and find out what other users are doing blah blah blah{" "}
+            </div>
           </div>
         </section>
-      )
+      );
     }
   }
 }
 
-export default Home
+export default Home;
